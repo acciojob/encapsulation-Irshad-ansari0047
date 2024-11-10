403ccef37b5722edfaac94100f71b383271feacb
Irshad-ansari0047 <irshadansari0047@gmail.com>
2024-11-11 01:23:32 +0530

    encapsulation achieved

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,18 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args) {
+    
+    RWOnly obj = new RWOnly();
+
+    // // directly issignining will give error due to private data member
+    // obj.name = "Irshad";
+
+    // //try printing but give error 
+    // System.out.println(obj.name);  // name is private member
+
+    obj.setName("Irshad");
+    System.out.println(obj.getName());
+
+  }
 }
